deb88676fd8ea9ea9dfa89fa53b391d1d4b94c87
Ankitha Bhat <137513282+Ankithakbhat@users.noreply.github.com>
2025-04-21 21:35:05 -0700

    Update apb_sequencer.sv

--- apb_sequencer.sv
+++ apb_sequencer.sv
@@ -1,9 +1,22 @@
-//`include "apb_sequence.sv"
+//======================================================//
+//              APB Sequencer Definition                //
+//======================================================//
+
+// This class is the sequencer in UVM. It controls the flow of apb_seq_item
+// objects from the sequence to the driver.
 
 class apb_sequencer extends uvm_sequencer#(apb_seq_item);
-`uvm_component_utils(apb_sequencer)
- function new(string name, uvm_component parent);
-      super.new(name, parent);
-   endfunction
+
+  //==================================================//
+  // UVM Factory Registration                         //
+  // Allows this component to be created using type names
+  `uvm_component_utils(apb_sequencer)
+
+  //==================================================//
+  // Constructor                                      //
+  // Initializes the sequencer and connects it to its parent in hierarchy
+  function new(string name, uvm_component parent);
+    super.new(name, parent); // Call the base class constructor
+  endfunction
 
 endclass
